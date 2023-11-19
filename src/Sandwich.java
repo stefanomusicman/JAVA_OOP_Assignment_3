fa49f12fc697be672aba8d9b9bcd67663e6ce259
Stefano <stef.pro@hotmail.com>
2023-11-18 22:02:09 -0500

    chore: created remaining classes for assignment

--- src/Sandwich.java
+++ src/Sandwich.java
@@ -1,4 +1,24 @@
 
-public class Sandwich {
-
+public class Sandwich extends Edible {
+	private String mainIngredient;
+	private String size;
+	
+	public Sandwich(String name, double price, int calories, String mainIngredient, String size) {
+		super(name, price, calories);
+		this.mainIngredient = mainIngredient;
+		this.size = size;
+	}
+	
+	public String getMainIngredient() {
+		return mainIngredient;
+	}
+	
+	public String getSize() {
+		return size;
+	}
+	
+	public String toString() {
+		return String.format("Product Name: %s   ,   Price: %s  ,   Calories: %s,   Expiry Date: %s,   Main Ingredient: %s,   Size: %s", 
+				getName(),getPrice(), getCalories(), getExpiryDate(), getMainIngredient(), getSize());
+	}
 }
