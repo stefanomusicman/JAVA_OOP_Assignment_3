bfa9cb1e3dbede34c1a91d74615e407c1473062e
Stefano <stef.pro@hotmail.com>
2023-11-21 17:54:44 -0500

    modified option 2

--- src/MenuOptions.java
+++ src/MenuOptions.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class MenuOptions {	
 	public static void optionOne(ArrayList<Product> productList, Scanner scanner) {
 		String chocolate;
+		double price;
 		int calories;
 		
 		while(true) {
@@ -13,6 +14,101 @@ public class MenuOptions {
 			
 			if(chocolate.trim().isEmpty()) {
 				System.out.println("Field cannot be empty.");
+			} else {
+				break;
+			}
+		}
+		
+		while(true) {
+			System.out.print("Price of the chocolate bar: ");
+			String input = scanner.nextLine();
+			
+			try {
+				price = Double.parseDouble(input);
+				break;
+			} catch(Exception e) {
+				System.out.println("Must be a number");
+			}
+			
+		}
+		
+		while(true) {
+			System.out.print("Enter the amount of calories: ");
+			String input = scanner.nextLine();
+			
+			if(input.trim().isEmpty()) {
+				System.out.println("Field cannot be empty.");
+				continue;
+			}
+			
+			try {
+				calories = Integer.parseInt(input);
+				
+				Chocolate chocolateBar = new Chocolate(chocolate, price, calories);
+				productList.add(chocolateBar);
+				System.out.println("Chocolate Bar has been added!");
+				break;
+			} catch(Exception e) {
+				System.out.println(e);
+			}
+		}
+	}
+	
+	public static void optionTwo(ArrayList<Product> productList, Scanner scanner) {
+		String name;
+		String mainIngredient;
+		String size;
+		double price;
+		int calories;
+		
+		while(true) {
+			System.out.print("Name of the sandwich: ");
+			name = scanner.nextLine();
+			
+			if(name.trim().isEmpty()) {
+				System.out.println("Field cannot be empty. ");
+			} else {
+				break;
+			}
+			
+		}
+		
+		while(true) {
+			System.out.print("What is the main ingredient of your sandwich: ");
+			mainIngredient = scanner.nextLine();
+
+			if(mainIngredient.trim().isEmpty()) {
+				System.out.print("Invalid input! Field cannot be empty! ");
+			} else if(!mainIngredient.equals("chicken") && !mainIngredient.equals("meat") && !mainIngredient.equals("veggie")) {
+				System.out.print("Please choose a valid ingredient (chicken, meat or veggie) ");
+				continue;
+			} else {
+				break;
+			}
+		}
+		
+		while(true) {
+			System.out.print("Price of the sandwich: ");
+			String input = scanner.nextLine();
+			
+			try {
+				price = Double.parseDouble(input);
+				break;
+			} catch(Exception e) {
+				System.out.println("Must be a number");
+			}
+			
+		}
+		
+		while(true) {
+			System.out.print("What is the size of your sandwich(small, medium, large): ");
+			size = scanner.nextLine();
+
+			if(size.trim().isEmpty()) {
+				System.out.print("Invalid input! Field cannot be empty! ");
+			} else if(!size.equals("small") && !size.equals("medium") && !size.equals("large")) {
+				System.out.print("Please choose a valid size, either medium or large. ");
+				continue;
 			} else {
 				break;
 			}
@@ -30,48 +126,14 @@ public class MenuOptions {
 			try {
 				calories = Integer.parseInt(input);
 				
-				Chocolate chocolateBar = new Chocolate("Chocolate Bar", 2.50, calories);
-				productList.add(chocolateBar);
-				System.out.println("Chocolate Bar has been added!");
+				Sandwich sandwich = new Sandwich(name, price, calories, mainIngredient, size);
+				productList.add(sandwich);
+				System.out.println("Sandwich has been successfully added!");
 				break;
 			} catch(Exception e) {
 				System.out.println(e);
 			}
-		}
-	}
-	
-	public static void optionTwo(ArrayList<Product> productList, Scanner scanner) {
-		String mainIngredient;
-		String size;
-		double price;
-		int calories;
-		
-		while(true) {
-			System.out.print("What is the main ingredient of your sandwich: ");
-			mainIngredient = scanner.nextLine();
-
-			if(mainIngredient.trim().isEmpty()) {
-				System.out.print("Invalid input! Field cannot be empty! ");
-			} else if(!mainIngredient.equals("chicken") && !mainIngredient.equals("meat") && !mainIngredient.equals("veggie")) {
-				System.out.print("Please choose a valid ingredient (chicken, meat or veggie) ");
-				continue;
-			} else {
-				break;
-			}
-		}
-		
-		while(true) {
-			System.out.print("What is the size of your sandwich(small, medium, large): ");
-			size = scanner.nextLine();
-
-			if(size.trim().isEmpty()) {
-				System.out.print("Invalid input! Field cannot be empty! ");
-			} else if(!size.equals("small") && !size.equals("medium") && !size.equals("large")) {
-				System.out.print("Please choose a valid size, either medium or large. ");
-				continue;
-			} else {
-				break;
-			}
+			
 		}
 		
 		
