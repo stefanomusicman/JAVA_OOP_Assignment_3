70cd17b5042616f67ab2af70a1cd9f92fe7d03d8
Stefano <stef.pro@hotmail.com>
2023-11-20 21:17:00 -0500

    chore: create option 1

--- src/Assignment_3.java
+++ src/Assignment_3.java
@@ -1,12 +1,16 @@
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Assignment_3 {
 
 	public static void main(String[] args) {
-		startMenu();
+		ArrayList<Product> ediblesList = new ArrayList<Product>();
+		ArrayList<Product> gasList = new ArrayList<Product>();
+		startMenu(ediblesList, gasList);
 	}
 
-	public static void startMenu() {
+	public static void startMenu(ArrayList<Product> foodList, ArrayList<Product> gasList) {
 		Scanner userInput = new Scanner(System.in);
 		
 		int menuSelect;
@@ -51,7 +55,11 @@ public class Assignment_3 {
 
 			switch (menuSelect) {
 			case 1:
-				System.out.println("1-chocolate bar");
+				optionOne(foodList);
+				// this will prompt the user to press any key before returning to the main menu
+				// it will allow them to see the confirmation message that the item had been added
+				System.out.println("Press Enter to return to main menu...");
+				userInput.nextLine();
 				break;
 			case 2:
 
@@ -92,5 +100,16 @@ public class Assignment_3 {
 		
 		System.out.println("Exiting the program.");
 		userInput.close();
+	}
+	
+	public static void optionOne(ArrayList<Product> foodList) {
+		// Create a random integer between 50 and 200 to represent the calories of a chocolate bar
+		Random random = new Random();
+		int randomNumber = random.nextInt(151) + 50;
+		
+		// Add the chocolate bar to the list
+		Edible chocolateBar = new Edible("Chocolate Bar", 2.50, randomNumber);
+		foodList.add(chocolateBar);
+		System.out.println("Chocolate Bar has been added!");
 	}
 }
